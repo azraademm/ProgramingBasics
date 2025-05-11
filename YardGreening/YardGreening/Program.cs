d980c71f8a8949724c490a58db2c9e84bcb807d9
azraademm <ejdera@abv.bg>
2025-05-11 23:49:38 +0300

    Add files via upload

---
+++ YardGreening/YardGreening/Program.cs
@@ -0,0 +1,8 @@
+﻿double kwadratni = double.Parse(Console.ReadLine());
+
+double price = kwadratni * 7.61;
+double otstypka = 0.18 * price;
+double fullPrice = price - otstypka;
+
+Console.WriteLine($"The final price is: {fullPrice} lv.");
+Console.WriteLine($"The discount is: {otstypka} lv.");
