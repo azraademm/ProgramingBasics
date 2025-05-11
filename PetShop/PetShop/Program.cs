5e04c16bad34c844c83b414ee53167ec19add673
azraademm <ejdera@abv.bg>
2025-05-11 23:48:11 +0300

    Add files via upload

---
+++ PetShop/PetShop/Program.cs
@@ -0,0 +1,4 @@
+﻿int packDog = int.Parse(Console.ReadLine());
+int packCat = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{packDog * 2.50 + packCat * 4} lv.");
---
+++ ProjectsCreation/ProjectsCreation/Program.cs
@@ -0,0 +1,4 @@
+﻿string name = Console.ReadLine();
+int countProject = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"The architect {name} will need {countProject * 3} hours to complete {countProject} project/s.");
